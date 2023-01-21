7de7e5e1e5274459337e4e12c53fb1430ace6089
Yena-Yun <cheryleduart@gmail.com>
2023-01-21 14:33:08 +0900

    start: sass 설치, global/mixin 파일 추가, color 및 폰트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h3 style={{ fontFamily: 'Shrikhand' }}>1</h3>
+      <h4 style={{ fontFamily: 'Hi Melody' }}>크리스피 프레시</h4>
+      <p style={{ fontFamily: 'Nanum Pen Script' }}>
+        집에서 가까운 곳. 자주 간다.
+      </p>
     </div>
   );
 }
