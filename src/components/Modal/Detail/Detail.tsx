9680e560573354df1309796463d6712138b62ff0
Yena-Yun <cheryleduart@gmail.com>
2023-01-30 19:57:59 +0900

    Detail 모달 작성 + css 추가

--- src/components/Modal/Detail/Detail.tsx
+++ src/components/Modal/Detail/Detail.tsx
@@ -1,15 +1,57 @@
-import { useRecoilValue } from 'recoil';
-import { transactionListState } from 'recoil/atom';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { clickedExpenseCardState, transactionListState } from 'recoil/atom';
+import { closeModalSelector } from 'recoil/selector';
 import { ModalLayout } from '../Layout/ModalLayout';
 import styles from './Detail.module.scss';
 
 export const Detail = () => {
+  const setCloseModal = useSetRecoilState(closeModalSelector);
   const transactionList = useRecoilValue(transactionListState);
+  const clickedExpenseCard = useRecoilValue(clickedExpenseCardState);
+
+  const { id, date, title, items } = transactionList.filter(
+    ({ id }) => id === clickedExpenseCard
+  )[0];
 
   return (
-    <ModalLayout>
-      <div></div>
-      <div></div>
+    <ModalLayout modalRole='detail'>
+      <h2 className={styles.modalTitle}>상세</h2>
+
+      <div className={styles.mainContainer}>
+        <div className={styles.header}>
+          <div className={styles.title}>{title}</div>
+          <div className={styles.date}>
+            {date.toLocaleString('ko-KR', {
+              year: 'numeric',
+              month: '2-digit',
+              day: '2-digit',
+            })}
+          </div>
+        </div>
+        <div className={styles.info}>
+          {items.map(({ id, name, tag, price }) => (
+            <div key={id} className={styles.nameTagGroup}>
+              <div className={styles.name}>
+                {name !== '' && '-'} &nbsp; {name}
+              </div>
+              <div className={styles.tag}>{tag}</div>
+              <div className={styles.price}>{price}</div>
+            </div>
+          ))}
+        </div>
+        <div className={styles.total}>
+          {items.map(({ price }) => price).reduce((acc, cur) => acc + cur)}
+        </div>
+      </div>
+
+      <div className={styles.confirmButtonContainer}>
+        <button
+          className={styles.confirmButton}
+          onClick={() => setCloseModal()}
+        >
+          확인
+        </button>
+      </div>
     </ModalLayout>
   );
 };
