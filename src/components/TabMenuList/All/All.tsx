792d7eee1c2c4dd976d97f5ca14e7be44b7887e9
Yena-Yun <cheryleduart@gmail.com>
2023-01-30 19:57:17 +0900

    클릭한 카드의 상세 모달 띄우기

--- src/components/TabMenuList/All/All.tsx
+++ src/components/TabMenuList/All/All.tsx
@@ -1,4 +1,9 @@
-import { useRecoilValue, useResetRecoilState, useSetRecoilState } from 'recoil';
+import {
+  useRecoilState,
+  useRecoilValue,
+  useResetRecoilState,
+  useSetRecoilState,
+} from 'recoil';
 import {
   clickedExpenseCardState,
   transactionListState,
@@ -7,40 +12,56 @@ import {
 import { HiOutlinePlusCircle } from 'react-icons/hi2';
 
 import styles from './All.module.scss';
-import { openModalSelector } from 'recoil/selector';
+import { openModalSelector, toggleDetailModalSelector } from 'recoil/selector';
+import { Detail } from 'components/Modal/Detail/Detail';
 
 export const All = () => {
   const transactionList = useRecoilValue(transactionListState);
   const setOpenModal = useSetRecoilState(openModalSelector);
   const resetTransactionList = useResetRecoilState(transactionState);
-  const clickedExpenseCard = useRecoilValue(clickedExpenseCardState);
+  const [clickedExpenseCard, setClickedExpenseCard] = useRecoilState(
+    clickedExpenseCardState
+  );
+  const [isOpenDetailModal, setIsOpenDetailModal] = useRecoilState(
+    toggleDetailModalSelector
+  );
 
   return (
     <div className={styles.container}>
       <ul className={styles.expenseItemList}>
         {transactionList.map(({ id, date, title, items }) => (
-          <li key={id} className={styles.expenseItem}>
-            <div className={styles.info}>
-              <div className={styles.date}>
-                {date.toLocaleString('ko-KR', {
-                  year: 'numeric',
-                  month: '2-digit',
-                  day: '2-digit',
-                })}
-              </div>
-              <div className={styles.title}>{title}</div>
-              <div className={styles.nameTagGroup}>
-                <div className={styles.name}>
-                  {items[0].name !== '' && '•'} {items[0].name}
+          <div key={id} className={styles.expenseItem}>
+            {clickedExpenseCard === id && <Detail />}
+            <li
+              onClick={() => {
+                setClickedExpenseCard(id);
+                setIsOpenDetailModal();
+              }}
+            >
+              <div className={styles.info}>
+                <div className={styles.date}>
+                  {date.toLocaleString('ko-KR', {
+                    year: 'numeric',
+                    month: '2-digit',
+                    day: '2-digit',
+                  })}
                 </div>
-                <div className={styles.tag}>{items[0].tag}</div>
-                {items.length > 1 && ` 외 +${items.length - 1}`}
+                <div className={styles.title}>{title}</div>
+                <div className={styles.nameTagGroup}>
+                  <div className={styles.name}>
+                    {items[0].name !== '' && '•'} {items[0].name}
+                  </div>
+                  <div className={styles.tag}>{items[0].tag}</div>
+                  {items.length > 1 && ` 외 +${items.length - 1}`}
+                </div>
               </div>
-            </div>
-            <div className={styles.price}>
-              {items.map(({ price }) => price).reduce((acc, cur) => acc + cur)}
-            </div>
-          </li>
+              <div className={styles.price}>
+                {items
+                  .map(({ price }) => price)
+                  .reduce((acc, cur) => acc + cur)}
+              </div>
+            </li>
+          </div>
         ))}
       </ul>
       <button
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,20 @@
 import { useRecoilValue } from 'recoil';
-import { isOpenModalState } from 'recoil/atom';
+import { clickedExpenseCardState, isOpenModalState } from 'recoil/atom';
 import { Header } from 'components/Header/Header';
 import { Main } from 'components/Main/Main';
 import { Modal } from 'components/Modal/Modal';
 import { TabMenu } from 'components/TabMenuList/Layout/TabMenu';
 import styles from './Home.module.scss';
+import { Detail } from 'components/Modal/Detail/Detail';
 
 export const Home = () => {
   const isOpenModal = useRecoilValue(isOpenModalState);
+  const clickedExpenseCard = useRecoilValue(clickedExpenseCardState);
 
   return (
     <>
       {isOpenModal && <Modal />}
+      {clickedExpenseCard && <Detail />}
       <div className={styles.container}>
         <Header />
         <div className={styles.innerContainer}>
