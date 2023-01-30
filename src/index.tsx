331332381d617fb4fff62f606349b48cd27ebbc7
Yena-Yun <cheryleduart@gmail.com>
2023-01-30 19:58:58 +0900

    사소한 처리 및 css 추가

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,4 @@ import './styles/globals.scss';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <>
-    <App />
-  </>
-);
+root.render(<App />);
