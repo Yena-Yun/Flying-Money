f6afd18707e0584c8be59de68b475e7cc8818d4c
Yena-Yun <cheryleduart@gmail.com>
2023-02-14 13:27:36 +0900

    Date, Title, List 분리한 AddModal 코드 정리

---
+++ src/components/Modal/Add/Add.tsx
@@ -0,0 +1,83 @@
+import {
+  useRecoilValue,
+  useSetRecoilState,
+  useRecoilState,
+  useResetRecoilState,
+} from 'recoil';
+import uuid4 from 'uuid4';
+import {
+  expenseListState,
+  isOpenCalendarState,
+  listState,
+  transactionState,
+} from 'recoil/atom';
+import {
+  closeModalSelector,
+  addTransactionListSelector,
+  addListSelector,
+} from 'recoil/selector';
+import { ModalLayout } from '../Layout/ModalLayout';
+import { Date } from './Date/Date';
+import { Title } from './Title/Title';
+import { List } from './List/List';
+import { Calendar } from 'components/Calendar/Calendar';
+import { ItemType, TransactionType } from 'types/types';
+import styles from './Add.module.scss';
+
+export const Add = () => {
+  const setCloseModal = useSetRecoilState(closeModalSelector);
+  const isOpenCalender = useRecoilValue(isOpenCalendarState);
+
+  const expenseItemList = useRecoilValue<ItemType[]>(expenseListState);
+  const resetExpenseItemList = useResetRecoilState(expenseListState);
+
+  const [list, setList] = useRecoilState(listState);
+  const setListToTransaction = useSetRecoilState(addListSelector);
+
+  const [transaction, setTransaction] =
+    useRecoilState<TransactionType>(transactionState);
+  const addTransactionList = useSetRecoilState(addTransactionListSelector);
+  const resetTransaction = useResetRecoilState(transactionState);
+
+  return (
+    <ModalLayout modalRole='add'>
+      {isOpenCalender && <Calendar />}
+      <h2 className={styles.title}>항목 등록하기</h2>
+
+      <div className={styles.mainContainer}>
+        <Date />
+        <Title />
+        <List />
+      </div>
+
+      <div className={styles.submitButtonWrap}>
+        <button
+          className={styles.submitButton}
+          onClick={() => {
+            setList({
+              ...list,
+              items: expenseItemList,
+            });
+
+            setListToTransaction();
+
+            setTransaction({
+              ...transaction,
+              id: uuid4(),
+              list: [...transaction.list, list],
+            });
+
+            addTransactionList();
+
+            resetExpenseItemList();
+            resetTransaction();
+
+            setCloseModal();
+          }}
+        >
+          등록
+        </button>
+      </div>
+    </ModalLayout>
+  );
+};
