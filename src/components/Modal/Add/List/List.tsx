0d764a61894e2ec447cfaac8d4f66ea9077c9312
Yena-Yun <cheryleduart@gmail.com>
2023-02-17 11:14:50 +0900

    항목 추가 클릭 시 아래에 빈 항목 추가

--- src/components/Modal/Add/List/List.tsx
+++ src/components/Modal/Add/List/List.tsx
@@ -1,32 +1,16 @@
-import {
-  useRecoilValue,
-  useSetRecoilState,
-  useRecoilState,
-  useResetRecoilState,
-} from 'recoil';
-import classnames from 'classnames';
-import {
-  clickedTagPopupIndexState,
-  itemState,
-  isOpenTagPopupState,
-} from 'recoil/atom';
-import { toggleTagPopupSelector } from 'recoil/selector';
-import { TagPopup } from 'components/Modal/TagPopup/TagPopup';
+import { useRecoilState } from 'recoil';
+import uuid4 from 'uuid4';
+import { itemState } from 'recoil/atom';
+import { Input } from './Input/Input';
 import { ItemType } from 'types/types';
 import { HiOutlinePlusCircle, HiOutlineMinusCircle } from 'react-icons/hi2';
 import styles from './List.module.scss';
 
 export const List = () => {
-  const isOpenTagPopup = useRecoilValue(isOpenTagPopupState);
-  const setOpenTagPopup = useSetRecoilState(toggleTagPopupSelector);
-  const [clickedTagPopupIndex, setClickedTagPopupIndex] = useRecoilState(
-    clickedTagPopupIndexState
-  );
   const [items, setItems] = useRecoilState<ItemType[]>(itemState);
-  const resetItemList = useResetRecoilState(itemState);
 
   const handleAddItem = () => {
-    console.log(items);
+    setItems((prev) => [...prev, { id: uuid4(), name: '', price: 0, tag: '' }]);
   };
 
   return (
@@ -35,61 +19,7 @@ export const List = () => {
 
       {items.map(({ id: index, tag }) => (
         <div key={index} className={styles.inputItemGroup}>
-          <div className={styles.inputGroup}>
-            <div className={classnames(styles.inputItem, styles.nameInput)}>
-              <input
-                placeholder='항목명'
-                onChange={(e) => {
-                  setItems(
-                    items.map((item) =>
-                      item.id === index
-                        ? { ...item, name: e.target.value }
-                        : item
-                    )
-                  );
-                }}
-              />
-            </div>
-
-            <div className={styles.priceTagGroup}>
-              <div className={classnames(styles.inputItem, styles.priceInput)}>
-                <input
-                  type='number'
-                  placeholder='가격'
-                  onChange={(e) => {
-                    if (isNaN(e.target.valueAsNumber)) return;
-
-                    setItems(
-                      items.map((item) =>
-                        item.id === index
-                          ? {
-                              ...item,
-                              price: parseInt(e.target.value),
-                            }
-                          : item
-                      )
-                    );
-                  }}
-                />
-              </div>
-
-              <div
-                className={styles.tagOpenButton}
-                data-id={index}
-                onClick={(e) => {
-                  setOpenTagPopup();
-                  setClickedTagPopupIndex(String(e.currentTarget.dataset.id));
-                }}
-              >
-                {tag ? (
-                  <div className={styles.tag}>{tag}</div>
-                ) : (
-                  <div className={styles.tag}>태그 등록하기</div>
-                )}
-              </div>
-            </div>
-            {clickedTagPopupIndex === index && isOpenTagPopup && <TagPopup />}
-          </div>
+          <Input index={index} tag={tag} />
 
           <div
             className={styles.removeItemButton}
