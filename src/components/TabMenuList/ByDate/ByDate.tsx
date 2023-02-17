4e3108a10ca4a677b6c070d13fec33fd384703ca
Yena-Yun <cheryleduart@gmail.com>
2023-02-17 15:04:11 +0900

    날짜별 탭(ByDate): Header 분리, All과 달리 Detail 한번에 확인할 수 있도록 css 수정

--- src/components/TabMenuList/ByDate/ByDate.tsx
+++ src/components/TabMenuList/ByDate/ByDate.tsx
@@ -1,29 +1,21 @@
-import { useRecoilValue, useSetRecoilState } from 'recoil';
-import classnames from 'classnames';
-import { CiCalendar } from 'react-icons/ci';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import {
   selectedDateState,
-  isOpenCalendarState,
   transactionListState,
+  clickedIndexState,
+  isOpenDetailModalState,
 } from 'recoil/atom';
-import { toggleCalendarSelector } from 'recoil/selector';
-import { WeekCalendar } from 'components/WeekCalendar/WeekCalendar';
+import { toggleModalSelector } from 'recoil/selector';
+import { Detail } from 'components/Modal/Detail/Detail';
+import { Header } from './Header/Header';
 import styles from './ByDate.module.scss';
-import { formatDateWeekday } from 'hooks/formatDate';
 
 export const ByDate = () => {
-  const isOpenCalender = useRecoilValue(isOpenCalendarState);
-  const setToggleCalendar = useSetRecoilState(toggleCalendarSelector);
+  const setOpenModal = useSetRecoilState(toggleModalSelector);
+  const isOpenDetailModal = useRecoilValue(isOpenDetailModalState);
   const selectedDate = useRecoilValue(selectedDateState);
   const transactionList = useRecoilValue(transactionListState);
-
-  const filterPriceOnDate = () => {
-    return transactionList
-      .filter(({ date }) => selectedDate.toString().includes(date.toString()))
-      .flatMap(({ lists }) =>
-        lists.flatMap(({ items }) => items.map(({ price }) => price))
-      );
-  };
+  const [clickedIndex, setClickedIndex] = useRecoilState(clickedIndexState);
 
   const filterTransactionOnDate = () => {
     return transactionList.filter(
@@ -32,47 +24,36 @@ export const ByDate = () => {
     );
   };
 
+  const openDetailModal = (id: string) => {
+    setClickedIndex(id);
+    setOpenModal('detail');
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <div className={classnames(styles.inputGroup, styles.period)}>
-          <div
-            className={classnames(styles.dateIcon, styles.icon)}
-            onClick={() => setToggleCalendar()}
-          >
-            <CiCalendar />
-          </div>
-          {isOpenCalender && <WeekCalendar />}
-          <div className={styles.selectedDate}>
-            {formatDateWeekday(selectedDate)}
-          </div>
-        </div>
-        <div className={classnames(styles.inputGroup, styles.totalExpense)}>
-          <div className={styles.expense}>
-            총 {filterPriceOnDate().reduce((acc, cur) => acc + cur, 0)}원
-          </div>
-        </div>
-      </div>
-      <div className={styles.filteredList}>
+      <Header />
+      <ul className={styles.filteredList}>
         {filterTransactionOnDate().map(({ id, lists }) =>
           lists.map(({ title, items }) => (
-            <li key={id} className={styles.expenseItem}>
-              <div className={styles.info}>
-                <div className={styles.title}>{title}</div>
-                <div className={styles.name}>
-                  {items[0].name !== '' && '•'} {items[0].name}
-                  {items.length > 1 && ` 외 +${items.length - 1}`}
+            <li
+              key={id}
+              className={styles.expenseItem}
+              onClick={() => openDetailModal(id)}
+            >
+              {clickedIndex === id && isOpenDetailModal && <Detail />}
+              <div className={styles.title}>{title}</div>
+              {items.map(({ name, price }) => (
+                <div className={styles.item}>
+                  <div className={styles.name}>
+                    {items[0].name !== '' && '•'} {name}
+                  </div>
+                  <div className={styles.price}>{price}</div>
                 </div>
-              </div>
-              <div className={styles.price}>
-                {items
-                  .map(({ price }) => price)
-                  .reduce((acc, cur) => acc + cur)}
-              </div>
+              ))}
             </li>
           ))
         )}
-      </div>
+      </ul>
     </div>
   );
 };
