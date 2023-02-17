6b081e72c6f1094e1d85da8932370003ea8f5773
Yena-Yun <cheryleduart@gmail.com>
2023-02-17 12:33:09 +0900

    Detail: JSX에서 함수 분리, formatDate 사용

--- src/components/Modal/Detail/Detail.tsx
+++ src/components/Modal/Detail/Detail.tsx
@@ -1,38 +1,42 @@
 import { useRecoilValue, useSetRecoilState } from 'recoil';
-import { clickedExpenseIndexState, transactionListState } from 'recoil/atom';
+import { clickedIndexState, transactionListState } from 'recoil/atom';
 import {
   deleteTransactionListSelector,
   toggleModalSelector,
 } from 'recoil/selector';
 import { ModalLayout } from '../Layout/ModalLayout';
+import { formatDate } from 'hooks/formatDate';
 import styles from './Detail.module.scss';
 
 export const Detail = () => {
   const setCloseModal = useSetRecoilState(toggleModalSelector);
   const transactionList = useRecoilValue(transactionListState);
-  const clickedExpenseCard = useRecoilValue(clickedExpenseIndexState);
+  const clickedIndex = useRecoilValue(clickedIndexState);
   const setDeleteTransaction = useSetRecoilState(deleteTransactionListSelector);
 
   const { date, lists } = transactionList.find(
-    ({ id }) => id === clickedExpenseCard
+    ({ id }) => id === clickedIndex
   )!;
+
+  const confirmTransaction = () => {
+    setCloseModal('detail');
+  };
+
+  const deleteTransaction = () => {
+    setDeleteTransaction();
+    setCloseModal('detail');
+  };
 
   return (
     <ModalLayout modalRole='detail'>
       <h2 className={styles.modalTitle}>상세</h2>
 
       <div className={styles.mainContainer}>
-        {lists.map(({ title, items }) => (
-          <>
+        {lists.map(({ id, title, items }) => (
+          <div key={id}>
             <div className={styles.header}>
               <div className={styles.title}>{title}</div>
-              <div className={styles.date}>
-                {date.toLocaleString('ko-KR', {
-                  year: 'numeric',
-                  month: '2-digit',
-                  day: '2-digit',
-                })}
-              </div>
+              <div className={styles.date}>{formatDate(date)}</div>
             </div>
             <div className={styles.info}>
               {items.map(({ id, name, tag, price }) => (
@@ -48,24 +52,15 @@ export const Detail = () => {
             <div className={styles.total}>
               {items.map(({ price }) => price).reduce((acc, cur) => acc + cur)}
             </div>
-          </>
+          </div>
         ))}
       </div>
 
       <div className={styles.actionButtonContainer}>
-        <button
-          className={styles.confirmButton}
-          onClick={() => setCloseModal('detail')}
-        >
+        <button className={styles.confirmButton} onClick={confirmTransaction}>
           확인
         </button>
-        <button
-          className={styles.deleteButton}
-          onClick={() => {
-            setDeleteTransaction();
-            setCloseModal('detail');
-          }}
-        >
+        <button className={styles.deleteButton} onClick={deleteTransaction}>
           삭제
         </button>
       </div>
