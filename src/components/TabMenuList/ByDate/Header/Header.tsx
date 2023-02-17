a7bc23403b9b6aa610d534e7cd56f9dc389fa6e3
Yena-Yun <cheryleduart@gmail.com>
2023-02-17 15:02:56 +0900

    날짜별 탭에서 Header 분리

---
+++ src/components/TabMenuList/ByDate/Header/Header.tsx
@@ -0,0 +1,48 @@
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import classnames from 'classnames';
+import {
+  selectedDateState,
+  isOpenCalendarState,
+  transactionListState,
+} from 'recoil/atom';
+import { toggleCalendarSelector } from 'recoil/selector';
+import { WeekCalendar } from 'components/WeekCalendar/WeekCalendar';
+import { formatDateWeekday } from 'hooks/formatDate';
+import { CiCalendar } from 'react-icons/ci';
+import styles from './Header.module.scss';
+
+export const Header = () => {
+  const isOpenCalender = useRecoilValue(isOpenCalendarState);
+  const setToggleCalendar = useSetRecoilState(toggleCalendarSelector);
+  const selectedDate = useRecoilValue(selectedDateState);
+  const transactionList = useRecoilValue(transactionListState);
+
+  const filterPriceOnDate = () => {
+    return transactionList
+      .filter(({ date }) => selectedDate.toString().includes(date.toString()))
+      .flatMap(({ lists }) =>
+        lists.flatMap(({ items }) => items.map(({ price }) => price))
+      );
+  };
+  return (
+    <div className={styles.header}>
+      <div className={classnames(styles.inputGroup, styles.period)}>
+        <div
+          className={classnames(styles.dateIcon, styles.icon)}
+          onClick={() => setToggleCalendar()}
+        >
+          <CiCalendar />
+        </div>
+        {isOpenCalender && <WeekCalendar />}
+        <div className={styles.selectedDate}>
+          {formatDateWeekday(selectedDate)}
+        </div>
+      </div>
+      <div className={classnames(styles.inputGroup, styles.totalExpense)}>
+        <div className={styles.expense}>
+          총 {filterPriceOnDate().reduce((acc, cur) => acc + cur, 0)}원
+        </div>
+      </div>
+    </div>
+  );
+};
