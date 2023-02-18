9246dd5401b3d1b513286b6580629dfdab944d0b
Yena-Yun <cheryleduart@gmail.com>
2023-02-18 12:06:30 +0900

    최상단에서 ByDateDetail 모달 열리게 하기

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,23 +3,29 @@ import {
   clickedIndexState,
   isOpenDetailModalState,
   isOpenAddModalState,
+  clickedItemIndexState,
+  isOpenByDateDetailModalState,
 } from 'recoil/atom';
 import { Header } from 'components/Header/Header';
 import { Main } from 'components/Main/Main';
 import { TabMenu } from 'components/TabMenuList/Layout/TabMenu';
 import styles from './Home.module.scss';
 import { Detail } from 'components/Modal/Detail/Detail';
+import { Detail as ByDateDetail } from 'components/TabMenuList/ByDate/Detail/Detail';
 import { Add } from 'components/Modal/Add/Add';
 
 export const Home = () => {
   const isOpenAddModal = useRecoilValue(isOpenAddModalState);
-  const clickedIndex = useRecoilValue(clickedIndexState);
   const isOpenDetailModal = useRecoilValue(isOpenDetailModalState);
+  const isOpenByDateDetailModal = useRecoilValue(isOpenByDateDetailModalState);
+  const clickedIndex = useRecoilValue(clickedIndexState);
+  const clickedItemIndex = useRecoilValue(clickedItemIndexState);
 
   return (
     <>
       {isOpenAddModal && <Add />}
-      {clickedIndex !== '' && isOpenDetailModal && <Detail />}
+      {clickedIndex && isOpenDetailModal && <Detail />}
+      {clickedItemIndex && isOpenByDateDetailModal && <ByDateDetail />}
       <div className={styles.container}>
         <Header />
         <div className={styles.innerContainer}>
