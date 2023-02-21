329909619740c0e7ec27a55e31fa4e64cecfd87b
Yena-Yun <cheryleduart@gmail.com>
2023-02-21 12:50:07 +0900

    formatDate 파일 내 함수 위치 이동

--- src/hooks/formatDate.tsx
+++ src/hooks/formatDate.tsx
@@ -6,15 +6,6 @@ export const formatDate = (date: Date) => {
   });
 };
 
-export const formatHeaderDate = (date: Date) => {
-  return date
-    .toLocaleString('ko-KR', {
-      hour: 'numeric',
-      minute: 'numeric',
-    })
-    .slice(2);
-};
-
 export const formatDateWeekday = (date: Date) => {
   return date.toLocaleString('ko-KR', {
     year: '2-digit',
@@ -22,4 +13,13 @@ export const formatDateWeekday = (date: Date) => {
     day: '2-digit',
     weekday: 'long',
   });
+};
+
+export const formatRootHeaderDate = (date: Date) => {
+  return date
+    .toLocaleString('ko-KR', {
+      hour: 'numeric',
+      minute: 'numeric',
+    })
+    .slice(2);
 };
