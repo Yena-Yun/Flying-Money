3bcb1e39dfa60cf13fa2a14ad01141789fb10995
Yena-Yun <cheryleduart@gmail.com>
2023-02-21 17:54:09 +0900

    Home import * as 처리, import문 변경

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,31 +1,28 @@
 import { useRecoilValue } from 'recoil';
-import {
-  clickedIndexState,
-  isOpenDetailModalState,
-  isOpenAddModalState,
-  clickedItemIndexState,
-  isOpenByDateDetailModalState,
-} from 'recoil/atom';
+import * as A from 'recoil/atom';
 import { Header } from 'components/Header/Header';
 import { Main } from 'components/Main/Main';
-import { TabMenu } from 'components/TabMenuList/Layout/TabMenu';
-import styles from './Home.module.scss';
-import { Detail } from 'components/Modal/Detail/Detail';
-import { Detail as ByDateDetail } from 'components/TabMenuList/ByDate/Detail/Detail';
 import { Add } from 'components/Modal/Add/Add';
+import { Detail } from 'components/Modal/Detail/Detail';
+import { TabMenu } from 'components/TabMenu/Layout/TabMenuLayout';
+import { ByDateDetail } from 'components/TabMenu/ByDate/Detail/Detail';
+import styles from './Home.module.scss';
 
 export const Home = () => {
-  const isOpenAddModal = useRecoilValue(isOpenAddModalState);
-  const isOpenDetailModal = useRecoilValue(isOpenDetailModalState);
-  const isOpenByDateDetailModal = useRecoilValue(isOpenByDateDetailModalState);
-  const clickedIndex = useRecoilValue(clickedIndexState);
-  const clickedItemIndex = useRecoilValue(clickedItemIndexState);
+  const isOpenAddModal = useRecoilValue(A.isOpenAddModalState);
+  const isOpenDetailModal = useRecoilValue(A.isOpenDetailModalState);
+  const isOpenByDateDetailModal = useRecoilValue(
+    A.isOpenByDateDetailModalState
+  );
+  const clickedIndex = useRecoilValue(A.clickedIndexState);
+  const clickedItemIndex = useRecoilValue(A.clickedItemIndexState);
 
   return (
     <>
       {isOpenAddModal && <Add />}
       {clickedIndex && isOpenDetailModal && <Detail />}
       {clickedItemIndex && isOpenByDateDetailModal && <ByDateDetail />}
+
       <div className={styles.container}>
         <Header />
         <div className={styles.innerContainer}>
