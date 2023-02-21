6632714ed744880a77830d993ab26701de517a78
Yena-Yun <cheryleduart@gmail.com>
2023-02-21 13:52:12 +0900

    CalendarIcon 추가

---
+++ src/components/Icons/Calendar/Calendar.tsx
@@ -0,0 +1,10 @@
+import { CiCalendar } from 'react-icons/ci';
+import styles from './Calendar.module.scss';
+
+export const CalendarIcon = () => {
+  return (
+    <div className={styles.icon}>
+      <CiCalendar />
+    </div>
+  );
+};
