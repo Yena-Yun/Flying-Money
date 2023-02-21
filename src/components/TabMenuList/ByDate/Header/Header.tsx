0a682edf9e52b228a9a48a12df1d32033cd8b73e
Yena-Yun <cheryleduart@gmail.com>
2023-02-21 12:52:37 +0900

    ByDate: Calendar open 관련 분기 추가

--- src/components/TabMenuList/ByDate/Header/Header.tsx
+++ src/components/TabMenuList/ByDate/Header/Header.tsx
@@ -2,17 +2,19 @@ import { useRecoilValue, useSetRecoilState } from 'recoil';
 import classnames from 'classnames';
 import {
   filterByDateSelectedDateState,
+  isOpenByDateCalendarState,
+  isOpenByDateDetailModalState,
   isOpenCalendarState,
   transactionListState,
 } from 'recoil/atom';
 import { toggleCalendarSelector } from 'recoil/selector';
-import { WeekCalendar } from 'components/WeekCalendar/WeekCalendar';
+import { MiniCalendar } from 'components/MiniCalendar/MiniCalendar';
 import { formatDateWeekday } from 'hooks/formatDate';
 import { CiCalendar } from 'react-icons/ci';
 import styles from './Header.module.scss';
 
 export const Header = () => {
-  const isOpenCalender = useRecoilValue(isOpenCalendarState);
+  const isOpenCalender = useRecoilValue(isOpenByDateCalendarState);
   const setToggleCalendar = useSetRecoilState(toggleCalendarSelector);
   const selectedDate = useRecoilValue(filterByDateSelectedDateState);
   const transactionList = useRecoilValue(transactionListState);
@@ -30,11 +32,11 @@ export const Header = () => {
       <div className={classnames(styles.inputGroup, styles.period)}>
         <div
           className={classnames(styles.dateIcon, styles.icon)}
-          onClick={() => setToggleCalendar()}
+          onClick={() => setToggleCalendar('byDate')}
         >
           <CiCalendar />
         </div>
-        {isOpenCalender && <WeekCalendar />}
+        {isOpenCalender && <MiniCalendar tabName='byDate' />}
         <div className={styles.selectedDate}>
           {formatDateWeekday(selectedDate)}
         </div>
