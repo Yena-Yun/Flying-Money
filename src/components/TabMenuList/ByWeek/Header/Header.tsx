58468f29121ab76c983d79148c4792bc02b781da
Yena-Yun <cheryleduart@gmail.com>
2023-02-21 13:50:37 +0900

    ByWeek 헤더에 안내문구 추가, CalendarIcon 컴포넌트로 대체

--- src/components/TabMenuList/ByWeek/Header/Header.tsx
+++ src/components/TabMenuList/ByWeek/Header/Header.tsx
@@ -1,13 +1,14 @@
+import { useState } from 'react';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import classnames from 'classnames';
 import { addDays } from 'date-fns';
-import { CiCalendar } from 'react-icons/ci';
 import { MiniCalendar } from 'components/MiniCalendar/MiniCalendar';
 import {
   filterByWeekStartDateState,
   isOpenByWeekCalendarState,
 } from 'recoil/atom';
 import { toggleCalendarSelector } from 'recoil/selector';
+import { CalendarIcon } from 'components/Icons/Calendar/Calendar';
 import { formatDateWeekday } from 'hooks/formatDate';
 import styles from './Header.module.scss';
 
@@ -15,23 +16,30 @@ export const Header = () => {
   const isOpenCalender = useRecoilValue(isOpenByWeekCalendarState);
   const setToggleCalendar = useSetRecoilState(toggleCalendarSelector);
   const selectedDate = useRecoilValue(filterByWeekStartDateState);
+  const [isSelectSomeDate, setIsSelectSomeDate] = useState(true);
 
   return (
     <div className={classnames(styles.inputGroup, styles.period)}>
-      <h3 className={styles.subTitle}>기간</h3>
       {isOpenCalender && <MiniCalendar tabName='byWeek' />}
-      <div
-        className={classnames(styles.dateIcon, styles.icon)}
-        onClick={() => setToggleCalendar('byWeek')}
-      >
-        <CiCalendar />
+
+      <div className={styles.period}>
+        <CalendarIcon />
+        <div
+          className={classnames(styles.startDate, styles.selectedDate)}
+          onClick={() => {
+            setToggleCalendar('byWeek');
+            setIsSelectSomeDate(false);
+          }}
+        >
+          {formatDateWeekday(selectedDate)}
+        </div>
+        -
+        <div className={styles.selectedDate}>
+          {formatDateWeekday(addDays(selectedDate, 6))}
+        </div>
       </div>
-      <div className={styles.selectedDate}>
-        {formatDateWeekday(selectedDate)}
-      </div>
-      -
-      <div className={styles.selectedDate}>
-        {formatDateWeekday(addDays(selectedDate, 6))}
+      <div className={styles.header}>
+        {isSelectSomeDate && <p>시작 날짜를 선택하세요.</p>}
       </div>
     </div>
   );
