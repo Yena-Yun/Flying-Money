d2bbf9d1b846723145a1dc15f8b62f359159dea1
Yena-Yun <cheryleduart@gmail.com>
2023-02-23 19:08:39 +0900

    Main 랜덤 문구 수정 및 추가

--- src/utils/constants/constants.ts
+++ src/utils/constants/constants.ts
@@ -3,7 +3,7 @@ import { TabMenuIdType, TabMenuStringType } from 'types/types';
 export const RANDOM_PHRASES = [
   {
     line1: 'Gone with the wind~',
-    line2: '이번 주도 바람과 함께 사라졌나요?',
+    line2: '이번 주는 어디로 날아갔나요?',
   },
   {
     line1: '돈을 많이 쓴 곳이 곧 당신이랍니다',
@@ -11,19 +11,23 @@ export const RANDOM_PHRASES = [
   },
   {
     line1: '지금 구매하면 30% 할인?',
-    line2: '아예 안 사면 100% 할인!',
+    line2: '사지 않으면 100% 할인~',
   },
   {
     line1: '돈은 있다가도 없고, 없다가도 있는 거라고요?',
     line2: '훗, 돈은 없으면 없는 거랍니다 :)',
   },
   {
-    line1: '지름신이 오시면 가슴에 손을 얹고 생각해보세요',
-    line2: '정말 필요한 것인지 아닌지',
+    line1: '지름신 내쫓는 법?',
+    line2: '정말 필요한지 다시 한 번 생각하기!',
   },
   {
-    line1: '오늘 쓴 1000원',
-    line2: '한 달이면 3만 원',
+    line1: '오늘 쓴 1000원?',
+    line2: '한 달이면 3만 원..!',
+  },
+  {
+    line1: '그 돈이면 떡볶이를 몇 그릇..?',
+    line2: '여러분이 좋아하는 걸로 환산해보세요~',
   },
 ];
 
