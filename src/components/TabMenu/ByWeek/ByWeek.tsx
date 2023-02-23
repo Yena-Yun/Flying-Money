4b40a334d99537685d96e0b4e68231f6e62169c5
Yena-Yun <cheryleduart@gmail.com>
2023-02-23 11:44:47 +0900

    주별 필터에 상세 버튼과 한 주 소감 textarea,수정 버튼 추가

--- src/components/TabMenu/ByWeek/ByWeek.tsx
+++ src/components/TabMenu/ByWeek/ByWeek.tsx
@@ -1,11 +1,12 @@
+import { useState } from 'react';
 import { useSetRecoilState } from 'recoil';
-import classnames from 'classnames';
-import styles from './ByWeek.module.scss';
-import { Header } from './Header/Header';
 import { toggleCalendarSelector } from 'recoil/selector';
+import { Header } from './Header/Header';
+import styles from './ByWeek.module.scss';
 
 export const ByWeek = () => {
   const setToggleCalendar = useSetRecoilState(toggleCalendarSelector);
+  const [isOpenTextarea, setIsOpenTextarea] = useState(false);
 
   return (
     <>
@@ -16,9 +17,39 @@ export const ByWeek = () => {
       <div className={styles.container}>
         <Header />
 
-        <div className={classnames(styles.inputGroup, styles.totalExpense)}>
-          <h3 className={styles.subTitle}>총 지출</h3>
-          <div className={styles.expense}>기간 내 지출 금액</div>
+        <div className={styles.totalExpense}>
+          <div className={styles.info}>
+            <h3 className={styles.subTitle}>총 지출</h3>
+            <div className={styles.expense}>기간 내 지출 금액</div>
+          </div>
+          <button className={styles.detailButton}>상세</button>
+        </div>
+        <div className={styles.weekDiary}>
+          <div className={styles.inputArea}>
+            <h3 className={styles.subTitle}>한 주 소감</h3>
+            {isOpenTextarea ? (
+              <textarea
+                className={styles.textarea}
+                rows={8}
+                cols={32}
+                onBlur={() => setIsOpenTextarea(false)}
+                autoFocus
+              >
+                이번 주는 무지출 데이가 많았다
+              </textarea>
+            ) : (
+              <p>이번 주는 무지출 데이가 많았다</p>
+            )}
+          </div>
+
+          {!isOpenTextarea && (
+            <button
+              className={styles.editButton}
+              onClick={() => setIsOpenTextarea(true)}
+            >
+              수정
+            </button>
+          )}
         </div>
       </div>
     </>
