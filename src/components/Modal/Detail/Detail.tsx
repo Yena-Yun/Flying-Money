0bde88033c122c25d5334d3e4aa36ea6a2b2b7ea
Yena-Yun <cheryleduart@gmail.com>
2023-02-23 16:44:47 +0900

    Detail과 ModalLayout css 수정

--- src/components/Modal/Detail/Detail.tsx
+++ src/components/Modal/Detail/Detail.tsx
@@ -9,6 +9,7 @@ import { ModalLayout } from '../Layout/ModalLayout';
 import { Toast } from '../Toast/Toast';
 import { formatDate } from 'utils/hooks/formatDate';
 import styles from './Detail.module.scss';
+import { formatMoney } from 'utils/hooks/formatMoney';
 
 export const Detail = () => {
   const isOpenToast = useRecoilValue(isOpenToastState);
@@ -21,45 +22,49 @@ export const Detail = () => {
     ({ id }) => id === clickedIndex
   )!;
 
-  const confirmTransaction = () => {
-    setCloseModal('detail');
-  };
-
   return (
     <>
       {isOpenToast && <Toast role='detail' />}
       <ModalLayout role='detail'>
         <h2 className={styles.modalTitle}>상세</h2>
+        <div className={styles.date}>{formatDate(date)}</div>
 
         <div className={styles.mainContainer}>
           {lists.map(({ id, title, items }) => (
             <div key={id}>
-              <div className={styles.header}>
-                <div className={styles.title}>{title}</div>
-                <div className={styles.date}>{formatDate(date)}</div>
-              </div>
-              <div className={styles.info}>
+              <div className={styles.title}>{title}</div>
+              <div className={styles.expenseList}>
                 {items.map(({ id, name, tag, price }) => (
-                  <div key={id} className={styles.nameTagGroup}>
-                    <div className={styles.name}>
-                      {name !== '' && '-'} &nbsp; {name}
+                  <div key={id} className={styles.expenseItem}>
+                    <div className={styles.nameTagGroup}>
+                      <div className={styles.name}>
+                        {name !== '' && '‣'} &nbsp; {name}&nbsp;
+                      </div>
+                      {tag && <div className={styles.tag}>{tag}</div>}
                     </div>
-                    {tag && <div className={styles.tag}>{tag}</div>}
-                    <div className={styles.price}>{price}</div>
+                    <div className={styles.price}>{formatMoney(price)}</div>
                   </div>
                 ))}
               </div>
               <div className={styles.total}>
-                {items
-                  .map(({ price }) => price)
-                  .reduce((acc, cur) => acc + cur)}
+                Total&nbsp;
+                <span>
+                  {formatMoney(
+                    items
+                      .map(({ price }) => price)
+                      .reduce((acc, cur) => acc + cur)
+                  )}
+                </span>
               </div>
             </div>
           ))}
         </div>
 
         <div className={styles.actionButtonContainer}>
-          <button className={styles.confirmButton} onClick={confirmTransaction}>
+          <button
+            className={styles.confirmButton}
+            onClick={() => setCloseModal('detail')}
+          >
             확인
           </button>
           <button
