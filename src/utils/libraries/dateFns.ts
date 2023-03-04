805b9a0120dfb3aae0e22705344e1c4d5afca4d3
Yena-Yun <cheryleduart@gmail.com>
2023-03-04 13:01:28 +0900

    dateFns export문 수정 (용량 같음)

--- src/utils/libraries/dateFns.ts
+++ src/utils/libraries/dateFns.ts
@@ -1,16 +1,3 @@
-import format from 'date-fns/format';
-import startOfMonth from 'date-fns/startOfMonth';
-import endOfMonth from 'date-fns/endOfMonth';
-import startOfWeek from 'date-fns/startOfWeek';
-import endOfWeek from 'date-fns/endOfWeek';
-import isSameMonth from 'date-fns/isSameMonth';
-import isSameDay from 'date-fns/isSameDay';
-import addDays from 'date-fns/addDays';
-import isMonday from 'date-fns/isMonday';
-import isWithinInterval from 'date-fns/isWithinInterval';
-import subDays from 'date-fns/subDays';
-import getWeeksInMonth from 'date-fns/getWeeksInMonth';
-
 export {
   format,
   startOfMonth,
@@ -24,4 +11,4 @@ export {
   isWithinInterval,
   subDays,
   getWeeksInMonth,
-};
+} from 'date-fns';
