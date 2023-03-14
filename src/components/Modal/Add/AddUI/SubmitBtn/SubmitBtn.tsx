0b28fd3f6afb1b8c35fa2924a580f084fde8d100
Yena-Yun <cheryleduart@gmail.com>
2023-03-14 16:32:42 +0900

    메서드에 알맞은 변수명으로 수정

--- src/components/Modal/Add/AddUI/SubmitBtn/SubmitBtn.tsx
+++ src/components/Modal/Add/AddUI/SubmitBtn/SubmitBtn.tsx
@@ -6,12 +6,9 @@ import { TMain } from 'types';
 import styles from './SubmitBtn.module.scss';
 
 export const SubmitBtn = () => {
-  const setCloseModal = useSetRecoilState(SOpen.toggleModalSelector);
   const list = useRecoilValue(AMain.listState);
   const items = useRecoilValue<TMain.ItemType[]>(AMain.itemState);
-  const resetItems = useResetRecoilState(AMain.itemState);
-  const resetList = useResetRecoilState(AMain.listState);
-  const resetTransaction = useResetRecoilState(AMain.transactionState);
+
   const setItemToList = useSetRecoilState(SMain.setItemToListSelector);
   const setListToTransaction = useSetRecoilState(
     SMain.setListToTransactionSelector
@@ -20,26 +17,29 @@ export const SubmitBtn = () => {
     SMain.setTransactionListSelector
   );
   const setTotalExpense = useSetRecoilState(SMain.getTotalPerDateSelector);
+
+  const resetItems = useResetRecoilState(AMain.itemState);
+  const resetList = useResetRecoilState(AMain.listState);
+  const resetTransaction = useResetRecoilState(AMain.transactionState);
+
   const setCurrentDateToAddModal = useSetRecoilState(
     SMain.setCurrentDateToAddModalSelector
   );
+  const setCloseModal = useSetRecoilState(SOpen.toggleModalSelector);
 
   const validateList = () => {
-    const noNameItem = items.some((item) => !item.name);
-    const noPriceItem = items.some((item) => !item.price);
+    const isNoName = items.some(({ name }) => !name);
+    const isNoPrice = items.some(({ price }) => !price);
 
     if (!list.title) {
       Hook.popupToast('제목을 입력해주세요!', Const.TOAST_ID.TITLE);
-    } else if (noNameItem && noPriceItem) {
-      Hook.popupToast(
-        '항목명과 가격을 하나 이상 입력해주세요!',
-        Const.TOAST_ID.ITEM
-      );
-    } else if (noNameItem) {
+    } else if (isNoName && isNoPrice) {
+      Hook.popupToast('항목명과 가격을 입력해주세요!', Const.TOAST_ID.ITEM);
+    } else if (isNoName) {
       Hook.popupToast('항목명을 입력해주세요!', Const.TOAST_ID.NAME);
-    } else if (noPriceItem) {
+    } else if (isNoPrice) {
       Hook.popupToast('가격을 입력해주세요!', Const.TOAST_ID.PRICE);
-    } else if (!noNameItem && !noPriceItem) {
+    } else if (!isNoName && !isNoPrice) {
       return true;
     }
   };
@@ -51,11 +51,14 @@ export const SubmitBtn = () => {
       setItemToList();
       setListToTransaction();
       setTransactionToTransactionList();
+
       setTotalExpense('all');
       setTotalExpense('byDate');
+
       resetItems();
       resetList();
       resetTransaction();
+
       setCurrentDateToAddModal();
       setCloseModal('addModal');
     }
