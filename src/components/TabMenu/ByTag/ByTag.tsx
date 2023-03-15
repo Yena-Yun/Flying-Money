b95e81d502eef9453c4e6a47630083616d035fc5
Yena-Yun <cheryleduart@gmail.com>
2023-03-15 12:48:14 +0900

    ByTag 삭제

--- src/components/TabMenu/ByTag/ByTag.tsx
+++
@@ -1,5 +0,0 @@
-import styles from './ByTag.module.scss';
-
-export const ByTag = () => {
-  return <div>준비 중입니다!</div>;
-};
--- src/components/TabMenu/index.ts
+++ src/components/TabMenu/index.ts
@@ -2,4 +2,3 @@ export { TabMenu } from './Layout/TabMenuLayout';
 export { All } from './All/All';
 export { ByDate } from './ByDate/ByDate';
 export { ByWeek } from './ByWeek/ByWeek';
-export { ByTag } from './ByTag/ByTag';
