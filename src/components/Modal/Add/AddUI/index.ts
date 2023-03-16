fc40a9dfde337cbe83632ff0cb256ebce9564076
Yena-Yun <cheryleduart@gmail.com>
2023-03-16 18:00:28 +0900

    TagPopup 컴포넌트 위치 이동

--- src/components/Modal/Add/AddUI/index.ts
+++ src/components/Modal/Add/AddUI/index.ts
@@ -1,5 +1,5 @@
 export { Date } from './Date/Date';
 export { Title } from './Title/Title';
 export { List } from './List/List';
-export { TagPopup } from './TagPopup/TagPopup';
 export { SubmitBtn } from './SubmitBtn/SubmitBtn';
+export { TagPopup } from './List/TagPopup/TagPopup';
