5503c113dec956f058ace4b3f3e440b9e9171645
Yena-Yun <cheryleduart@gmail.com>
2023-03-20 19:54:50 +0900

    Add.tsx 코드 순서 조절

--- src/components/Modal/Add/Add.tsx
+++ src/components/Modal/Add/Add.tsx
@@ -1,35 +1,33 @@
 import { useEffect, useRef } from 'react';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
-import { AIndex, AMain, AOpen } from 'recoil/atom';
+import { useDebouncedCallback } from 'use-debounce';
+import { AIndex, AMain, AOpen, AUtil } from 'recoil/atom';
 import { SMain, SOpen } from '~/recoil/selector';
-import { ModalLayout } from '../Layout/ModalLayout';
-import { Calendar } from 'components/Calendar/Calendar';
 import { Date } from './Date/Date';
 import { SubmitBtn } from './SubmitBtn/SubmitBtn';
 import { Input } from '~/components/Shared/Input/Input';
 import { RemoveItem } from './ActionButton/RemoveItem/RemoveItem';
 import { AddItem } from './ActionButton/AddItem/AddItem';
+import { ModalLayout } from '../Layout/ModalLayout';
+import { Calendar } from 'components/Calendar/Calendar';
 import { blockInvalidInput } from '~/utils/hooks';
 import styles from './Add.module.scss';
-import { addedInputGroupCountState } from '~/recoil/atom/utilState';
-import { useDebouncedCallback } from 'use-debounce';
 
 export const AddModal = () => {
   const isOpenCalender = useRecoilValue(AOpen.isOpenAddCalendarState);
-  const saveTagGroup = useRecoilValue(AMain.savedTagGroupState);
-  const bottomRef = useRef<HTMLDivElement>(null);
+  const addedCount = useRecoilValue(AUtil.addedInputGroupCountState);
 
   const setTitle = useSetRecoilState(SMain.addModalTitleSelector);
   const list = useRecoilValue(AMain.addModalListState);
   const setList = useSetRecoilState(SMain.addModalListSelector);
-
-  const addedCount = useRecoilValue(addedInputGroupCountState);
+  const saveTagGroup = useRecoilValue(AMain.savedTagGroupState);
 
   const setClickedTagPopupIndex = useSetRecoilState(
     AIndex.clickedTagPopupIndexState
   );
   const setOpenTagPopup = useSetRecoilState(SOpen.toggleModalSelector);
   const isOpenNewModal = useRef(false);
+  const bottomRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (addedCount < 1) return;
@@ -48,7 +46,7 @@ export const AddModal = () => {
       flag,
       input: value,
     });
-  }, 500);
+  }, 400);
 
   return (
     <ModalLayout role='addModal'>
