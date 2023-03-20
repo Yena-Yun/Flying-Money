baa995a11a5b506a0cbb177a089292b0ccedd687
Yena-Yun <cheryleduart@gmail.com>
2023-03-20 18:57:10 +0900

    항목 2개 이상 추가 시 오른쪽에 삭제버튼 띄우기 위한 flag atom 추가

--- src/recoil/atom/index.ts
+++ src/recoil/atom/index.ts
@@ -2,3 +2,4 @@ export * as AMain from './mainState';
 export * as AOpen from './openState';
 export * as ADate from './dateState';
 export * as AIndex from './indexState';
+export * as AUtil from './utilState';
---
+++ src/recoil/atom/utilState.ts
@@ -0,0 +1,6 @@
+import { atom } from 'recoil';
+
+export const addedInputGroupCountState = atom({
+  key: 'addedInputGroupCount',
+  default: 0,
+});
