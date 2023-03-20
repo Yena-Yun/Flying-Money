f3a37f67154e0c9d153f154dbe11ac7307f88554
Yena-Yun <cheryleduart@gmail.com>
2023-03-20 18:55:40 +0900

    addModal 인풋 입력 debounce로 최적화

--- src/components/Modal/Add/Add.tsx
+++ src/components/Modal/Add/Add.tsx
@@ -1,14 +1,18 @@
-import { useEffect, useRef, useState } from 'react';
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { useEffect, useRef } from 'react';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { AIndex, AMain, AOpen } from 'recoil/atom';
+import { SMain, SOpen } from '~/recoil/selector';
 import { ModalLayout } from '../Layout/ModalLayout';
 import { Calendar } from 'components/Calendar/Calendar';
 import { Date } from './Date/Date';
 import { SubmitBtn } from './SubmitBtn/SubmitBtn';
-import { AddItem, Input, RemoveItem } from './AddUI';
+import { Input } from '~/components/Shared/Input/Input';
+import { RemoveItem } from './ActionButton/RemoveItem/RemoveItem';
+import { AddItem } from './ActionButton/AddItem/AddItem';
 import { blockInvalidInput } from '~/utils/hooks';
-import { SMain, SOpen } from '~/recoil/selector';
 import styles from './Add.module.scss';
+import { addedInputGroupCountState } from '~/recoil/atom/utilState';
+import { useDebouncedCallback } from 'use-debounce';
 
 export const AddModal = () => {
   const isOpenCalender = useRecoilValue(AOpen.isOpenAddCalendarState);
@@ -19,6 +23,8 @@ export const AddModal = () => {
   const list = useRecoilValue(AMain.addModalListState);
   const setList = useSetRecoilState(SMain.addModalListSelector);
 
+  const addedCount = useRecoilValue(addedInputGroupCountState);
+
   const setClickedTagPopupIndex = useSetRecoilState(
     AIndex.clickedTagPopupIndexState
   );
@@ -26,15 +32,23 @@ export const AddModal = () => {
   const isOpenNewModal = useRef(false);
 
   useEffect(() => {
-    if (list.length < 2) return;
+    if (addedCount < 1) return;
     bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
-  }, [list]);
+  }, [addedCount]);
 
   const selectTagHandler = (e: React.MouseEvent<HTMLDivElement>) => {
     setOpenTagPopup('tagPopup');
     setClickedTagPopupIndex(String(e.currentTarget.dataset.id));
     isOpenNewModal.current = true;
   };
+
+  const debounced = useDebouncedCallback(({ id, flag, value }) => {
+    setList({
+      id,
+      flag,
+      input: value,
+    });
+  }, 500);
 
   return (
     <ModalLayout role='addModal'>
@@ -50,6 +64,7 @@ export const AddModal = () => {
           <Date />
 
           <div className={styles.inputGroupWrap}>
+            {/* 제목 입력란 */}
             <label className={styles.subTitle}>제목</label>
             <Input
               name='title'
@@ -60,6 +75,7 @@ export const AddModal = () => {
             />
           </div>
 
+          {/* 항목 입력란 리스트 */}
           <div className={styles.inputGroupWrap}>
             <label className={styles.subTitle}>항목</label>
             {list.map(({ id, tag }) => {
@@ -72,32 +88,30 @@ export const AddModal = () => {
                         className={styles.nameInput}
                         placeholder='항목명'
                         onChange={(e) =>
-                          setList({
+                          debounced({
                             id,
                             flag: 'name',
-                            input: e.target.value,
+                            value: e.target.value,
                           })
                         }
                       />
                     </div>
 
                     <div className={styles.priceTagGroup}>
-                      {/* <div className={styles.inputItem}> */}
                       <Input
                         type='number'
                         name='price'
                         className={styles.priceInput}
                         placeholder='가격'
                         onKeyDown={blockInvalidInput}
-                        onChange={(e) => {
-                          setList({
+                        onChange={(e) =>
+                          debounced({
                             id,
                             flag: 'price',
-                            input: e.target.value,
-                          });
-                        }}
+                            value: e.target.value,
+                          })
+                        }
                       />
-                      {/* </div> */}
 
                       <div
                         className={styles.tagOpenButton}
@@ -110,6 +124,8 @@ export const AddModal = () => {
                       </div>
                     </div>
                   </div>
+
+                  {/* 개별 항목 삭제 버튼 */}
                   <RemoveItem index={id} />
                 </div>
               );
@@ -118,6 +134,8 @@ export const AddModal = () => {
 
           {/* '항목 추가' 버튼 */}
           <AddItem />
+
+          {/* 항목이 추가되면 스크롤 생김 */}
           <div ref={bottomRef} />
         </div>
 
