5fb897126d363f63e5def521807e4b1752fefa17
Yena-Yun <cheryleduart@gmail.com>
2023-03-21 19:34:17 +0900

    bottomRef 스크롤 별도의 상태(AUtil) 추가 삭제

--- src/components/Modal/Add/AddUI/ActionIcon/RemoveItem/RemoveItem.tsx
+++ src/components/Modal/Add/AddUI/ActionIcon/RemoveItem/RemoveItem.tsx
@@ -1,5 +1,5 @@
 import { useRecoilValue, useSetRecoilState } from 'recoil';
-import { AMain, AUtil } from '~/recoil/atom';
+import { AMain } from '~/recoil/atom';
 import { SModal } from '~/recoil/selector';
 import { HiOutlineMinusCircle as MinusIcon } from 'react-icons/hi2';
 import styles from './RemoveItem.module.scss';
@@ -9,14 +9,12 @@ interface RemoveItemProp {
 }
 
 export const RemoveItem = ({ index }: RemoveItemProp) => {
-  const setAddedCount = useSetRecoilState(AUtil.addedInputGroupCountState);
   const list = useRecoilValue(AMain.addModalListState);
   const setAddOrDeleteItem = useSetRecoilState(
     SModal.addOrDeleteItemInAddModalSelector
   );
 
   const removeItemHandler = () => {
-    setAddedCount((prev: number) => prev - 1);
     setAddOrDeleteItem({
       flag: 'deleteItem',
       index,
