60a1fd716cef7521f3de81d654053f57ab588660
Yena-Yun <cheryleduart@gmail.com>
2023-03-21 17:39:32 +0900

    불필요한 로직 삭제

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import { useRecoilValue } from 'recoil';
 import { AOpen, AIndex } from 'recoil/atom';
 import { Banner, Header } from 'components/Main';
 import { ManageTag, TabMenu } from 'components/TabMenu';
-import { AddModal, AllDetail, ByDateDetail, Toast } from 'components/Modal';
+import { AddModal, AllDetail, ByDateDetail } from 'components/Modal';
 import styles from './Home.module.scss';
 import { TagPopup } from '~/components/Modal/Add/TagPopup/TagPopup';
 
@@ -16,7 +16,6 @@ export const Home = () => {
   const clickedIndex = useRecoilValue(AIndex.clickedTransactionIndexState);
   const clickedItemIndex = useRecoilValue(AIndex.clickedListIndexState);
   const isOpenTagPopup = useRecoilValue(AOpen.isOpenTagPopupState);
-  const isOpenDeleteTagToast = useRecoilValue(AOpen.isOpenDeleteTagToastState);
 
   return (
     <>
@@ -25,9 +24,6 @@ export const Home = () => {
       {clickedItemIndex && isOpenByDateDetailModal && <ByDateDetail />}
       {isOpenTagModal && <ManageTag />}
       {isOpenTagPopup && <TagPopup />}
-      {isOpenDeleteTagToast && <Toast role='deleteTag' />}
-      {isOpenDeleteTagToast && <Toast role='deleteByAll' />}
-      {isOpenDeleteTagToast && <Toast role='deleteByDate' />}
 
       <div className={styles.container}>
         <Header />
