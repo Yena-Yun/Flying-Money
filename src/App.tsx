f4f7e2a3d8a894ae4be1abf2132ecefedfb105e1
Yena-Yun <cheryleduart@gmail.com>
2023-04-15 17:24:35 +0900

    body에 .content로 전체 페이지 너비/높이 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { Home } from 'pages/Home/Home';
 
 function App() {
-  return <Home />;
+  return (
+    <div className='content'>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
