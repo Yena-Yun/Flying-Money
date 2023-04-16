314a9fc6f7913010ba1b45fa2caebacc29577362
Yena-Yun <cheryleduart@gmail.com>
2023-04-16 17:45:54 +0900

    월별/주별 탭의 소감 수정 기능 추가

--- src/components/TabMenu/ByWeek/ByWeek.tsx
+++ src/components/TabMenu/ByWeek/ByWeek.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, ChangeEvent, MouseEvent } from 'react';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import classNames from 'classnames';
 import { AMain } from 'recoil/atom';
@@ -9,8 +9,23 @@ import styles from './ByWeek.module.scss';
 
 export const ByWeek = () => {
   const [isOpenTextarea, setIsOpenTextarea] = useState(false);
+  const [textareaInput, setTextareaInput] = useState('');
+  const [weekDiary, setWeekDiary] = useState('');
   const weekTotal = useRecoilValue(AMain.totalPerWeekState);
   const setToggleCalendar = useSetRecoilState(SOpen.toggleCalendarSelector);
+
+  const TEXTAREA_DEFAULT = '한 주 소감을 작성해보세요!';
+
+  const handleEditText = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setTextareaInput(e.currentTarget.value);
+  };
+
+  const submitEditText = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setWeekDiary(textareaInput);
+    setTextareaInput('');
+    setIsOpenTextarea((prev) => !prev);
+  };
 
   return (
     <>
@@ -39,27 +54,27 @@ export const ByWeek = () => {
                 className={styles.textarea}
                 rows={8}
                 cols={32}
-                onChange={() => {}}
-                onBlur={() => setIsOpenTextarea(false)}
+                value={textareaInput}
+                onChange={handleEditText}
                 autoFocus
               >
-                이번 주는 무지출 데이가 많았다
+                {textareaInput}
               </textarea>
             ) : (
-              <p className={styles.editedText}>
-                이번 주는 무지출 데이가 많았다
-              </p>
+              <p className={styles.editedText}>{weekDiary}</p>
+            )}
+
+            {!weekDiary && !isOpenTextarea && (
+              <p className={styles.editedText}>{TEXTAREA_DEFAULT}</p>
             )}
           </div>
 
-          {!isOpenTextarea && (
-            <button
-              className={classNames(styles.actionButton, styles.editButton)}
-              onClick={() => setIsOpenTextarea(true)}
-            >
-              수정
-            </button>
-          )}
+          <button
+            className={classNames(styles.actionButton, styles.editButton)}
+            onClick={submitEditText}
+          >
+            {!isOpenTextarea ? '수정' : '제출'}
+          </button>
         </div>
       </div>
     </>
