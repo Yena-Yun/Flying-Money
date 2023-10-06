b5a615131e6177139d7d79f24d5cf7a1c0a39696
Yena-Yun <cheryleduart@gmail.com>
2023-10-06 16:13:13 +0900

    Style: 메인 화면의 margin, padding 수정

--- src/components/TabMenu/All/All.tsx
+++ src/components/TabMenu/All/All.tsx
@@ -42,7 +42,8 @@ export const All = () => {
               />
             </picture>
             <p className={styles.defaultGuide}>
-              '태그 관리'에서 태그를 먼저 등록 후 <br />새 항목을 등록해주세요!
+              상단 '태그 관리' 메뉴에서 <br /> 사용하실 태그를 먼저
+              등록해주세요!
             </p>
           </div>
         ) : (
